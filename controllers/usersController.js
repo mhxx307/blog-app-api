6580e1b5d4ad8243c680f2677907bedb7b11a7ea
mhxx307 <minhquan.lavo@gmail.com>
2022-11-26 18:01:43 +0700

    update readme , finish get user

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -44,6 +44,16 @@ const usersController = {
             res.status(401).json("You can delete only your account");
         }
     },
+
+    getUser: async (req, res) => {
+        try {
+            const user = await User.findById(req.params.id);
+            const { password, ...others } = user._doc;
+            res.status(200).json(others);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    },
 };
 
 module.exports = usersController;
--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -4,5 +4,6 @@ const router = require("express").Router();
 
 router.put("/:id", usersController.updateUser);
 router.delete("/:id", usersController.deleteUser);
+router.get("/:id", usersController.getUser);
 
 module.exports = router;
